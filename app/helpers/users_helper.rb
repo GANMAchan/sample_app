e297a5bff210ab061fa9d89b4ff79d1ccf1b6b02
Kaito Ohmura <>
2023-11-16 21:26:53 +0900

    7章まで

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+  end
+
+  def new
+    @user = User.new
+  end
 end
